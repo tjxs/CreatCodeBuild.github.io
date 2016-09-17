14c9d6b83fa9042ede9a54881c310d8ce8a5705c
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2016-09-17 01:53:23 -0400

    + merge sort

--- js/algorithm.js
+++ js/algorithm.js
@@ -40,78 +40,46 @@ var Algorithm = {
     console.log('wall', wall, end);
     swap(array, wall, end);
     yield wall;
+  },
+
+  merge_sort: function merge_sort(array) {
+    if (array.length() > 1) {
+      var m = Math.floor(array.length() / 2);
+      var left = array.slice(0, m);
+      var right = array.slice(m);
+      this.merge_sort(left);
+      this.merge_sort(right);
+      this.merge(array, left, right);
+    }
+  },
+
+  merge: function merge(array, leftArray, rightArray) {
+    var i = 0;
+    var l_i = 0;
+    var r_i = 0;
+    while(l_i < leftArray && r_i < rightArray.length) {
+      if(leftArray[l_i] < rightArray[r_i]) {
+        array[i] = leftArray[l_i];
+        l_i += i;
+      } else {
+        array[i] = rightArray[r_i];
+        r_i += i;
+      }
+      i++;
+    }
+    // copy the remaining
+    while(l_i < leftArray.length) {
+      array[i] = leftArray[l_i];
+      l_i += 1;
+      i += 1;
+    }
+    while(r_i < rightArray.length) {
+      array[i] = rightArray[r_i];
+      r_i += 1;
+      i += 1;
+    }
   }
 };
 
 
-/* 3D */
-function smaller_than(cube1, cube2) {
-  return cube1.geometry.parameters.height < cube2.geometry.parameters.height;
-}
 
-function larger_than(cube1, cube2) {
-  return cube1.geometry.parameters.height > cube2.geometry.parameters.height;
-}
-
-function swap(array, a, b) {
-  var x = array[a].position.x;
-  array[a].position.setX(array[b].position.x);
-  array[b].position.setX(x);
-  var temp = array[a];
-  array[a] = array[b];
-  array[b] = temp;
-}
-
-
-// add a column to scene
-function create_column(columns, pos, height) {
-  var geometry = new THREE.BoxGeometry( 0.5, height, 0.5 );
-  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  var cube = new THREE.Mesh( geometry, material );
-  cube.position.set(pos[0], pos[1], pos[2]);
-  columns.push(cube);
-}
-
-function init_columns(numColumns) {
-  columns = [];
-  for(var i = 0; i < numColumns; i++) {
-    create_column(columns, [i*2-Math.floor(numColumns/2)*2, 0, 0], numColumns - i);
-  }
-  return columns;
-}
-
-function add_all_to(scene, columns) {
-  for(var c in columns) {
-    // console.log(columns[c])
-    scene.add(columns[c]);
-  }
-}
-
-function print(columns) {
-  for(var i = 0; i < columns.length; i++) {
-    console.log(columns[i].geometry.parameters.height, columns[i].position.x);
-    // console.log();
-  }
-}
-
-
-/* Start */
-
-function render() {
-  requestAnimationFrame( render );
-  renderer.render( scene, camera );
-  quickSortGenerator.next();
-}
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-var renderer = new THREE.WebGLRenderer();
-var columns = init_columns(50);
-add_all_to(scene, columns);
-
-var quickSortGenerator = Algorithm.quick_sort(columns, 0, columns.length-1, swap, smaller_than);
-
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-camera.position.z = 100;
-
-render();
