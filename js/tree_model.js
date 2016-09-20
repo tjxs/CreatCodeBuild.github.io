8ed5fdcf235819c21bdd78b34b3105c572272d73
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2016-09-20 02:33:34 -0400

    tree model

---
+++ js/tree_model.js
@@ -0,0 +1,13 @@
+/*
+ * The view-model of a binary tree? Controller? Adapter?
+ * I don't really know the right term
+ **/
+
+/*
+ * @leafDistance: positive number, the distance between 2 leaves
+ * @rootPos: the (x, y, z) position of root
+ * @facing: a (x, y, z) vector that defines the direction the tree is facing
+ **/
+function TreeModel(leafDistance, rootPos, facing) {
+	
+}
