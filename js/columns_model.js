c754e37186eb2ebeea8a1f4e559a564207fc4a27
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2016-09-27 01:14:31 -0400

    +

--- js/columns_model.js
+++ js/columns_model.js
@@ -5,33 +5,6 @@
  */
 
 
-function smaller_than(cube1, cube2) {
-  // console.log(123);
-  // console.log(cube1.position.x, cube2.position.x);
-  return cube1.geometry.parameters.height < cube2.geometry.parameters.height;
-}
-
-function larger_than(cube1, cube2) {
-  return cube1.geometry.parameters.height > cube2.geometry.parameters.height;
-}
-
-function swap(array, a, b) {
-  // var x = array[a].position.x;
-  // array[a].position.setX(array[b].position.x);
-  // array[b].position.setX(x);
-  var temp = array[a];
-  array[a] = array[b];
-  array[b] = temp;
-}
-
-function move_to(fromEle, array, toIndex) {
-  // var x = toIndex*2-100;
-  // console.log('before move', fromEle.position.x, x);
-  // fromEle.position.setX(x);
-  array[toIndex] = fromEle;
-  // console.log('after move', fromEle.position.x);
-}
-
 
 /*
  * @begin: 2-d array [x, z]; the begining point of x and z
@@ -39,12 +12,6 @@ function move_to(fromEle, array, toIndex) {
  * return a columns model
  */
 function ColumnsModel(begin, orientation) {
-
-	// todo: TreeModel and ColumnsModel share common pattern. Consider making them prototypical inherited?
-
-  // if(orientation !== [0,1] || orientation !== [1,0] || orientation !== [0,-1] || orientation !== [-1,0]) {
-  //   throw Error('orientation value error');
-  // }
 
   var model = {};
   model.columns = [];
@@ -58,10 +25,8 @@ function ColumnsModel(begin, orientation) {
   **/
   // let count = 1;
   function justify_position() {
-    // console.log('justify_position', count++);
     for(var index in model.columns) {
       if(!is_at_right_pos(index)) {
-        // console.log('not right', model.columns[index].position.x, rightPos);
 	      var rightPos = right_pos_of_column(index);
         model.columns[index].position.x = rightPos[0];
 	      model.columns[index].position.z = rightPos[1];
